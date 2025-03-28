9fde146443af347a892682e00a77192cee1feec4
JoeMinKyung <mk981015@naver.com>
2025-03-28 10:53:45 +0900

    feat: test 코드 없이 실행

--- src/main/java/com/example/cacheproject/domain/openapi/fetchstatus/repository/OpenApiFetchStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cacheproject.domain.openapi.fetchstatus.repository;
-
-
-import com.example.cacheproject.domain.openapi.fetchstatus.entity.OpenApiFetchStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OpenApiFetchStatusRepository extends JpaRepository<OpenApiFetchStatus, Long> {
-    OpenApiFetchStatus findTopByOrderByUpdatedAtDesc();  // 가장 최근에 업데이트된 데이터를 찾는 메서드
-}
--- src/main/java/com/example/cacheproject/domain/openapi/repository/OpenApiRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cacheproject.domain.openapi.repository;
-
-import com.example.cacheproject.domain.openapi.entity.OpenApi;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OpenApiRepository extends JpaRepository<OpenApi, Long> {
-}
